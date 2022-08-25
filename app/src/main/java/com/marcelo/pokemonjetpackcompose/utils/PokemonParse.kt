3f2d336173313631498da7864972575cbfad0874
Marcelo Santos <rocha.mer21@gmail.com>
2022-08-25 13:25:25 -0300

    feat: add Color for pokemon types

---
+++ app/src/main/java/com/marcelo/pokemonjetpackcompose/utils/PokemonParse.kt
@@ -0,0 +1,55 @@
+package com.marcelo.pokemonjetpackcompose.utils
+
+import androidx.compose.ui.graphics.Color
+import com.marcelo.pokemonjetpackcompose.model.Stat
+import com.marcelo.pokemonjetpackcompose.model.Type
+import com.marcelo.pokemonjetpackcompose.ui.theme.*
+import java.util.*
+
+fun parseTypeToColor(type: Type): Color {
+    return when (type.type.name.lowercase(Locale.ROOT)) {
+        "normal" -> TypeNormal
+        "fire" -> TypeFire
+        "water" -> TypeWater
+        "electric" -> TypeElectric
+        "grass" -> TypeGrass
+        "ice" -> TypeIce
+        "fighting" -> TypeFighting
+        "poison" -> TypePoison
+        "ground" -> TypeGround
+        "flying" -> TypeFlying
+        "psychic" -> TypePsychic
+        "bug" -> TypeBug
+        "rock" -> TypeRock
+        "ghost" -> TypeGhost
+        "dragon" -> TypeDragon
+        "dark" -> TypeDark
+        "steel" -> TypeSteel
+        "fairy" -> TypeFairy
+        else -> Color.Black
+    }
+}
+
+fun parseStatToColor(stat: Stat): Color {
+    return when (stat.stat.name.lowercase()) {
+        "hp" -> HPColor
+        "attack" -> AtkColor
+        "defense" -> DefColor
+        "special-attack" -> SpAtkColor
+        "special-defense" -> SpDefColor
+        "speed" -> SpdColor
+        else -> Color.White
+    }
+}
+
+fun parseStatToAbbr(stat: Stat): String {
+    return when (stat.stat.name.lowercase()) {
+        "hp" -> "HP"
+        "attack" -> "Atk"
+        "defense" -> "Def"
+        "special-attack" -> "SpAtk"
+        "special-defense" -> "SpDef"
+        "speed" -> "Spd"
+        else -> ""
+    }
+}
